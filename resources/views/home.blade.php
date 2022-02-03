691920e1d8a7e49d4b9b9bc5d8cca7e5a7349dbd
Alex Almeida Rocha <alexalmeidarocha@outlook.com.br>
2022-02-03 00:27:36 -0300

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,16 +11,16 @@
                 <a href="/">Home</a>
             </li>
             <li>                
-                <a href="sobre">Sobre Alex</a>
+                <a href="https://www.linkedin.com/in/alex-almeida-24049a213/">Sobre Alex</a>
             </li>
         </ul>
     </div>   
 </head>
 <body>
     <div>
-        <h1>Olá Mundo</h1>
+        <h1>Formulario de Aniversario</h1>
     </div>
-    <form name="calculo" method="post" action="">
+    <form name="calculo" method="POST" action="">
         <div>
             <div>                
                 <label for="title">Informe seu nome:</label>
@@ -34,41 +34,43 @@
                 <label for="title">Informe a data do seu nascimento:</label>
             </div>
             <div>
-                <!-- <input type="date" class="from-control" id="data" name="data" value=""/> -->
                 <input type="number" min = "1"max = "31" class="from-control" placeholder="Dia "id="dia_nascimento" name="dia_nascimento" value=""/> / <input type="number" min = "1"max="12" class="from-control" placeholder="Mês "id="mes_nascimento" name="mes_nascimento" value=""/> / <input type="number" min = "1900"max="2999" class="from-control" placeholder="Ano "id="ano_nascimento" name="ano_nascimento" value=""/>
             </div>
             <p>
-                <input type="text" class="from-control" readonly="readonly" id="resultado" name="resultado" value=""/>
+                <input type="text" class="from-control" readonly="readonly" id="resultado" name="resultado" value="" style="width: 35%;" />
             </p>
         </div>         
     </form>
     <div>
-        <button>Resultado</button>                  
+        <button id='btn_form' >Resultado</button>                  
     </div>
-    
 </body>
 </html>
+
 <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <script type="text/javascript">
 
     jQuery(function ($) {
-        $("button").click(function () {
-            var nome = "", dia_nascimento = 0, mes_nascimento = 0, ano_nascimento = 0, idade = 0; 
+        $("#btn_form").click(function () {
+            
             var a = new Date,
             ano_atual = a.getFullYear(),
             mes_atual = a.getMonth(),
             dia_atual = a.getDate();
 
-            nome = $("input[name=nome]").val();
-            dia_nascimento = $("input[name=dia_nascimento]").val();
-            mes_nascimento = $("input[name=mes_nascimento]").val();
-            ano_nascimento = $("input[name=ano_nascimento]").val();
+            var nome = $("#nome").val();
+            var dia_nascimento = $("#dia_nascimento").val();
+            var mes_nascimento = $("#mes_nascimento").val();
+            var ano_nascimento = $("#ano_nascimento").val();
             
-            idade = ano_atual - ano_nascimento;
+            var idade = ano_atual - ano_nascimento;
+
             if (mes_atual < mes_nascimento || mes_atual == mes_nascimento && dia_atual < dia_nascimento)
                 idade -= 1;
-            $("input[name=resultado]").val("Meu nome é " +nome+", nasci em "+dia_nascimento+"/"+mes_nascimento+"/"+ano_nascimento+" e minha idade é "+idade+" anos.");
-            return false;
+
+            $("#resultado").val("Meu nome é " + nome +", nasci em " + dia_nascimento + "/" + mes_nascimento + "/" + ano_nascimento + " e minha idade é " + idade + " anos.");
+
+            alert("Meu nome é " + nome +", nasci em " + dia_nascimento + "/" + mes_nascimento + "/" + ano_nascimento + " e minha idade é " + idade + " anos.");
         });
     });
 </script>
